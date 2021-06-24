3a1e158c7a54457fd96045cecb2d993b8d48d4bf
HuyNguyen2509 <85873321+HuyNguyen2509@users.noreply.github.com>
2021-06-24 19:09:03 +0700

    Update Bai2.js

--- js/Bai2.js
+++ js/Bai2.js
@@ -1,8 +1,14 @@
-var number1 = 10;
-var number2 = 20;
-var number3 = 14;
-var number4 = 9;
-var number5 = 99;
+var number1 = 1.1;
+var number2 = 2.2;
+var number3 = 3.3;
+var number4 = 4.4;
+var number5 = 5.5;
+
+number1 = Number(parseFloat(number1));
+number2 = Number(parseFloat(number2));
+number3 = Number(parseFloat(number3));
+number4 = Number(parseFloat(number4));
+number5 = Number(parseFloat(number5));
 
 var trungBinhNumber = (number1 + number2 + number3 + number4 + number5) / 5;
 
